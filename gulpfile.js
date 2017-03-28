f64e27b1da10d7d36c73a4dbb5c3c22643316a19
Alessandro Mitelli <alessandro.mitelli@outlook.com>
2017-03-28 13:38:43 +0200

    Added gulp-sass to compile sass into css. Also added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,34 @@ var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('minify', function() {
-    return gulp.src('./src/*.css')
-        .pipe(concat('mp.css'))
+var opts = {
+    autoprefixer: {
+        browsers: ["last 2 version", "> 1%", "ie 7"],
+        remove: false
+    }
+};
+
+// compile sass into css
+gulp.task('compile', function() {
+    return gulp.src('./src/*.scss')
+        .pipe(concat('mp.scss'))
         .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(opts.autoprefixer))
         .pipe(sourcemaps.write())
-        .pipe(rename('mp.min.css'))
         .pipe(gulp.dest('dist'));
 });
+
+// compress
+gulp.task('minify', function() {
+    return gulp.src('./dist/mp.css')
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
+
+// generate dist files
+gulp.task('build', ['compile', 'minify']);
